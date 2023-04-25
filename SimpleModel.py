9daf8e358d8218b81c9ab09db84cd47b465e38b2
emmamoerkpedersen <102033084+emmamoerkpedersen@users.noreply.github.com>
2023-04-25 22:17:06 +0200

    New model and plots

--- SimpleModel.py
+++ SimpleModel.py
@@ -1,38 +1,44 @@
 import numpy as np
 
-
+states = {}
+outflow_dict = ()
 ####################################
 #Inflow upper zone
 def simple_model(par,pnames,data_input):
     import lib_model as lb
-    global outflow_u, outflow_l, outflow_s, outflow_Y1C, baseflow
-    global states_l, states_s, states_u, states_Y1C
     # Soil storage
+    
     Smaxsoil=par[pnames.index('Smaxsoil')];msoil=par[pnames.index('msoil')];betasoil=par[pnames.index('betasoil')];S0soil=par[pnames.index('S0soil')]
     cf=par[pnames.index('cf')] #crop factor for regulating ET
     outflow_u, states_u = lb.unit_soil_zone_storage(cf*data_input['PET'].to_numpy(),data_input['Precipitation'].to_numpy(),[Smaxsoil,msoil,betasoil,S0soil],return_ET=False)
     # Flow from other Y1C
-    global percolation
     Smax_Y1C=par[pnames.index('Smax_Y1C')];PERC=par[pnames.index('PERC')];k0=par[pnames.index('k0')]; k1=par[pnames.index('k1')];S0_s=par[pnames.index('S0_s')]
     outflow_Y1C, percolation ,states_Y1C = lb.unit_hbv_shallow_storage(data_input['flowY1C'].to_numpy(),[Smax_Y1C,PERC,k0,k1,S0_s])
     #####################
     #add shallow storage
-    global percolation_s
     Smax_s=par[pnames.index('Smax_s')]
     outflow_s, percolation_s, states_s = lb.unit_hbv_shallow_storage(outflow_u,[Smax_s,PERC,k0,k1,S0_s])
     #print(outflow_u)
     #print(len(outflow_u))
+
     # add lower storage
-    S0_l = par[pnames.index('S0_l')]; k2 = par[pnames.index('k2')]
-    outflow_l, states_l = lb.unit_hbv_lower_storage(outflow_s,[S0_l,k2]) 
+    #S0_l = par[pnames.index('S0_l')]; k2 = par[pnames.index('k2')]
+    #outflow_l, states_l = lb.unit_hbv_lower_storage(outflow_s,[S0_l,k2]) 
     #
     tp=par[pnames.index('tp')];k=par[pnames.index('k')]
     # Modelling transport
-    streamflow=lb.unit_hydrograph(outflow_u+outflow_s+outflow_l+outflow_Y1C,[tp, k])
+    streamflow=lb.unit_hydrograph(outflow_u+outflow_s+outflow_Y1C,[tp, k])
     #
     baseflow=par[pnames.index('baseflow')]
     #
     streamflow=streamflow+baseflow
+    
+    states['states_u'] = states_u
+    states['states_Y1C'] = states_Y1C
+    states['states_s'] = states_s
+    #states['states_l'] = states_l
+    
+    
 
     return(streamflow)
 
@@ -78,3 +84,15 @@ def train_validate_test_split(df, train_percent=.7, validate_percent=.2, seed=No
     test = df.loc[perm[validate_end:]]
     test = test.sort_values('date')
     return train, validate, test
+
+
+#### Code only to make state plot
+# par_estimate_unscaled,ssetrace=ddsoptim.ddsoptim(sse,p0,pmax,pmin,2500,0.2,True,pscale,pnames,data_in)
+# import matplotlib.pyplot as plt
+
+# fig, axs = plt.subplots(len(states), 1, figsize=(10, 10))
+# for i, (key, value) in enumerate(states.items()):
+#     axs[i].plot(value['S'])
+#     axs[i].set_title(key)
+# plt.tight_layout()
+# plt.show()
